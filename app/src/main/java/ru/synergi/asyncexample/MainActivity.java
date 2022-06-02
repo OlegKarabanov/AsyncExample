276038e45f15d4b0b892118c2b2e151c043fa7cd
OlegKarabanov <salem27@mail.ru>
2022-06-02 20:21:32 +0300

    Created AsyncTaskExample activity

--- app/src/main/java/ru/synergi/asyncexample/MainActivity.java
+++ app/src/main/java/ru/synergi/asyncexample/MainActivity.java
@@ -2,7 +2,10 @@ package ru.synergi.asyncexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void buttonClick(View view){
+        switch(view.getId()){  //привязывыаем реализацию онклик на кнопку
+            case R.id.asynctask:
+                Intent intent = new Intent(this, AsyncTaskExample.class);
+                startActivity(intent); // вызываем интент
+        }
     }
 }
